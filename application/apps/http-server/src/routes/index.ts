4497fe2113314d449f25b6b26193d5ce661c2a94
harshit Mishra <miananya2@gmail.com>
2025-01-27 04:45:13 +0530

    other routes

--- application/apps/http-server/src/routes/index.ts
+++ application/apps/http-server/src/routes/index.ts
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import client from "@repo/db/client";
+import { Request, Response, Router } from "express";
 import adminRouter from "./admin.routes";
 import spaceRouter from "./space.routes";
 import userRouter from "./user.routes";
@@ -8,6 +9,30 @@ export const router = Router();
 router.route("/signup").post(Signup);
 
 router.route("/signin").post(signin);
+
+router.route("/element").get(async (req: Request, res: Response) => {
+  const elements = await client.element.findMany();
+  res.json({
+    elements: elements.map((element) => ({
+      id: element.id,
+      imageUrl: element.imageUrl,
+      width: element.width,
+      height: element.height,
+      static: element.static,
+    })),
+  });
+});
+
+router.route("/avatar").get(async (req: Request, res: Response) => {
+  const avatars = await client.avatar.findMany();
+  res.json({
+    avatars: avatars.map((avatar) => ({
+      id: avatar.id,
+      name: avatar.name,
+      imageUrl: avatar.imageUrl,
+    })),
+  });
+});
 
 router.use("/admin", adminRouter);
 router.use("/space", spaceRouter);
